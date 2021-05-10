2c24e9db2857c84f159455f01716a8df5c2436df
James Carn <james@jamescarn.co.uk>
2021-05-10 21:54:54 +0100

    django-api-v1.5
    added a couple of fields and populted them with information

--- django-api/myapi/api/models.py
+++ django-api/myapi/api/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=250)
     alias = models.CharField(max_length=250)
+    weakness = models.CharField(max_length=250)
+    backstory = models.TextField(max_length=2000)
+
 
     def __str__(self):
         return self.name
